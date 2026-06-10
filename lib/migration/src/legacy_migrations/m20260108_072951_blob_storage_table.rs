404b66ee78aa1ab4382716112322c5a6479e7ed7
Developer <info@procivis.ch>
2026-06-10 09:15:59 +0200

    ONE-10223: fix blob migration

--- lib/migration/src/legacy_migrations/m20260108_072951_blob_storage_table.rs
+++ lib/migration/src/legacy_migrations/m20260108_072951_blob_storage_table.rs
@@ -1,8 +1,11 @@
-use sea_orm::DatabaseBackend;
+use sea_orm::{DatabaseBackend, FromQueryResult};
 use sea_orm_migration::prelude::*;
 use sea_orm_migration::schema::string;
 
 use crate::datatype::{ColumnDefExt, timestamp, uuid_char};
+use crate::legacy_migrations::m20240110_000001_initial::Proof;
+use crate::legacy_migrations::m20250729_114143_proof_blob::Proof as ProofWithBlobId;
+use crate::legacy_migrations::m20251105_121212_waa_and_wua_blobs::Credential;
 
 #[derive(DeriveMigrationName)]
 pub struct Migration;
@@ -47,6 +50,53 @@ enum BlobStorage {
 
 async fn sqlite_migration(manager: &SchemaManager<'_>) -> Result<(), DbErr> {
     let db = manager.get_connection();
+
+    // blob references get cleared when the original table gets deleted,
+    // recover the entries after blob table modifications
+    #[derive(FromQueryResult, Debug)]
+    struct CredentialWithBlobs {
+        id: String,
+        credential_blob_id: Option<String>,
+        wallet_app_attestation_blob_id: Option<String>,
+        wallet_unit_attestation_blob_id: Option<String>,
+    }
+    let credentials = CredentialWithBlobs::find_by_statement(
+        manager.get_database_backend().build(
+            Query::select()
+                .from(Credential::Table)
+                .columns([
+                    Credential::Id,
+                    Credential::CredentialBlobId,
+                    Credential::WalletAppAttestationBlobId,
+                    Credential::WalletUnitAttestationBlobId,
+                ])
+                .cond_where(
+                    Cond::any()
+                        .add(Expr::col(Credential::CredentialBlobId).is_not_null())
+                        .add(Expr::col(Credential::WalletAppAttestationBlobId).is_not_null())
+                        .add(Expr::col(Credential::WalletUnitAttestationBlobId).is_not_null()),
+                ),
+        ),
+    )
+    .all(db)
+    .await?;
+
+    #[derive(FromQueryResult, Debug)]
+    struct ProofWithBlob {
+        id: String,
+        proof_blob_id: Option<String>,
+    }
+    let proofs = ProofWithBlob::find_by_statement(
+        manager.get_database_backend().build(
+            Query::select()
+                .from(Proof::Table)
+                .column(Proof::Id)
+                .column(ProofWithBlobId::ProofBlobId)
+                .cond_where(Expr::col(ProofWithBlobId::ProofBlobId).is_not_null()),
+        ),
+    )
+    .all(db)
+    .await?;
 
     db.execute_unprepared("PRAGMA defer_foreign_keys = ON;")
         .await?;
@@ -108,6 +158,38 @@ async fn sqlite_migration(manager: &SchemaManager<'_>) -> Result<(), DbErr> {
 
     db.execute_unprepared("PRAGMA defer_foreign_keys = OFF;")
         .await?;
+
+    for credential in credentials {
+        manager
+            .exec_stmt(
+                Query::update()
+                    .table(Credential::Table)
+                    .value(Credential::CredentialBlobId, credential.credential_blob_id)
+                    .value(
+                        Credential::WalletAppAttestationBlobId,
+                        credential.wallet_app_attestation_blob_id,
+                    )
+                    .value(
+                        Credential::WalletUnitAttestationBlobId,
+                        credential.wallet_unit_attestation_blob_id,
+                    )
+                    .cond_where(Expr::col(Credential::Id).eq(&credential.id))
+                    .to_owned(),
+            )
+            .await?;
+    }
+
+    for proof in proofs {
+        manager
+            .exec_stmt(
+                Query::update()
+                    .table(Proof::Table)
+                    .value(ProofWithBlobId::ProofBlobId, proof.proof_blob_id)
+                    .cond_where(Expr::col(Proof::Id).eq(&proof.id))
+                    .to_owned(),
+            )
+            .await?;
+    }
 
     Ok(())
 }
