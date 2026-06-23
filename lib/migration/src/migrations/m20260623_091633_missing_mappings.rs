92cb03b54572133b30e11860b1693a4aa226a61d
Developer <info@procivis.ch>
2026-06-23 21:09:12 +0800

    ONE-10429: refactor migration

--- lib/migration/src/migrations/m20260623_091633_missing_mappings.rs
+++ lib/migration/src/migrations/m20260623_091633_missing_mappings.rs
@@ -3,6 +3,7 @@ use std::collections::HashMap;
 use sea_orm::FromQueryResult;
 use sea_orm_migration::prelude::*;
 
+use crate::legacy_migrations::batch_utils::get_ids;
 use crate::migrations::m20260417_150300_initial::{ClaimSchema, CredentialSchema};
 use crate::migrations::m20260429_120000_credential_schema_multiformat::{
     CredentialSchemaFormat, CredentialSchemaFormatClaimSchema,
@@ -18,36 +19,51 @@ pub struct Migration;
 #[async_trait::async_trait]
 impl MigrationTrait for Migration {
     async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
-        let missing_mappings = find_missing_mappings(manager).await?;
-        tracing::debug!("Found {} missing mappings", missing_mappings.len());
+        let credential_schema_ids: Vec<String> = get_ids(
+            manager,
+            Query::select()
+                .column(CredentialSchema::Id)
+                .from(CredentialSchema::Table),
+        )
+        .await?;
 
-        let mut schemas: HashMap<
-            String, /* format_id */
-            (SchemaFormat, Vec<ClaimSchemaEntry>),
-        > = Default::default();
-        for missing_mapping in missing_mappings {
-            schemas
-                .entry(missing_mapping.format_id)
-                .or_insert((
-                    SchemaFormat {
-                        credential_schema_id: missing_mapping.credential_schema_id,
-                        format: missing_mapping.format,
-                        layout_properties: missing_mapping.layout_properties,
-                    },
-                    vec![],
-                ))
-                .1
-                .push(ClaimSchemaEntry {
-                    id: missing_mapping.claim_schema_id,
-                    key: missing_mapping.key,
-                    metadata: missing_mapping.metadata,
-                });
-        }
+        tracing::debug!("Found {} credential schemas", credential_schema_ids.len());
 
-        tracing::debug!("Affected {} schema-formats", schemas.len());
+        for credential_schema_id in credential_schema_ids {
+            let missing_mappings = find_missing_mappings(manager, &credential_schema_id).await?;
 
-        for (format_id, (schema, claim_schemas)) in schemas {
-            migrate_schema(manager, format_id, schema, claim_schemas).await?;
+            let mut schemas: HashMap<
+                String, /* format_id */
+                (SchemaFormat, Vec<ClaimSchemaEntry>),
+            > = Default::default();
+            for missing_mapping in missing_mappings {
+                schemas
+                    .entry(missing_mapping.format_id)
+                    .or_insert((
+                        SchemaFormat {
+                            format: missing_mapping.format,
+                            layout_properties: missing_mapping.layout_properties,
+                        },
+                        vec![],
+                    ))
+                    .1
+                    .push(ClaimSchemaEntry {
+                        id: missing_mapping.claim_schema_id,
+                        key: missing_mapping.key,
+                        metadata: missing_mapping.metadata,
+                    });
+            }
+
+            for (format_id, (schema, claim_schemas)) in schemas {
+                migrate_schema(
+                    manager,
+                    &credential_schema_id,
+                    format_id,
+                    schema,
+                    claim_schemas,
+                )
+                .await?;
+            }
         }
 
         Ok(())
@@ -56,14 +72,12 @@ impl MigrationTrait for Migration {
 
 #[derive(Debug)]
 struct SchemaFormat {
-    credential_schema_id: String,
     format: String,
     layout_properties: Option<String>,
 }
 
 #[derive(FromQueryResult, Debug)]
 struct SchemaFormatClaim {
-    credential_schema_id: String,
     format_id: String,
     claim_schema_id: String,
     format: String,
@@ -74,14 +88,11 @@ struct SchemaFormatClaim {
 
 async fn find_missing_mappings(
     manager: &SchemaManager<'_>,
+    credential_schema_id: &str,
 ) -> Result<Vec<SchemaFormatClaim>, DbErr> {
     SchemaFormatClaim::find_by_statement(
         manager.get_database_backend().build(
             Query::select()
-                .expr_as(
-                    Expr::col((CredentialSchema::Table, CredentialSchema::Id)),
-                    "credential_schema_id",
-                )
                 .expr_as(
                     Expr::col((CredentialSchemaFormat::Table, CredentialSchemaFormat::Id)),
                     "format_id",
@@ -111,6 +122,10 @@ async fn find_missing_mappings(
                         .equals((ClaimSchema::Table, ClaimSchema::CredentialSchemaId)),
                 )
                 .cond_where(
+                    Expr::col((CredentialSchema::Table, CredentialSchema::Id))
+                        .eq(credential_schema_id),
+                )
+                .and_where(
                     Expr::tuple([
                         Expr::col((CredentialSchemaFormat::Table, CredentialSchemaFormat::Id))
                             .into(),
@@ -132,10 +147,16 @@ async fn find_missing_mappings(
 
 async fn migrate_schema(
     manager: &SchemaManager<'_>,
+    credential_schema_id: &str,
     format_id: String,
     schema: SchemaFormat,
     claim_schemas: Vec<ClaimSchemaEntry>,
 ) -> Result<(), DbErr> {
+    tracing::debug!(
+        "Migrating schema({credential_schema_id}, format: {})",
+        schema.format
+    );
+
     let is_mdoc = schema.format.starts_with("MDOC");
 
     // create mappings
@@ -146,7 +167,7 @@ async fn migrate_schema(
     }
 
     if is_mdoc && let Some(layout_properties) = schema.layout_properties {
-        migrate_mdoc_layout_props(manager, layout_properties, &schema.credential_schema_id).await?;
+        migrate_mdoc_layout_props(manager, layout_properties, credential_schema_id).await?;
     }
 
     Ok(())
